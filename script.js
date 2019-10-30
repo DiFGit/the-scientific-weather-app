da660d37fd2dcd87594ee2c5a038697dfedb3517
DiFGit <55925077+DiFGit@users.noreply.github.com>
2019-10-30 23:12:44 +0000

    added realtime weather details
    namely wind, humidity and pressure.
    changed as well units for wind speed accordingly with metrics (m/s) and imperial (mph).

--- script.js
+++ script.js
@@ -38,22 +38,25 @@ function displayData(response) {
   let currentLocalTempMax = Math.round(response.data.main.temp_max);
   let currentLocalTempMin = Math.round(response.data.main.temp_min);
   let currentLocalTempAmplitude = document.querySelector("#tempMaxMin");
-  //let currentLocalWind = document.querySelector("#");
-  //let currentLocalHumidity = document.querySelector("#");
-  //let currentLocalPressure = document.querySelector("#");
+  let currentLocalWind = document.querySelector("#windSpeed");
+  let currentLocalHumidity = document.querySelector("#humidity");
+  let currentLocalPressure = document.querySelector("#pressure");
   let currentCity = document.querySelector("#location");
   currentCity.innerHTML = response.data.name;
   currentTemperatureElement.innerHTML = `${temperature}`;
   currentLocalWeatherDes.innerHTML = response.data.weather[0].description;
   currentLocalTempAmplitude.innerHTML = `${currentLocalTempMax}º / ${currentLocalTempMin}º`;
-  //currentLocalWind.innerHTML = response.data.wind.speed;
-  //currentLocalHumidity.innerHTML = response.data.main.humidity;
-  //currentLocalPressure.innerHTML = response.data.main.pressure;
+  currentLocalWind.innerHTML = response.data.wind.speed;
+  currentLocalHumidity.innerHTML = response.data.main.humidity;
+  currentLocalPressure.innerHTML = response.data.main.pressure;
 }
 function displayByDefault() {
+  let windUnits = document.querySelector("#windUnits");
+  windUnits.innerHTML = `m/s`;
   let apiKey = "1c79a9c19394dbdbf78cd6d4344cc928";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayData);
+  console.log(apiUrl);
 }
 displayByDefault();
 
@@ -63,6 +66,8 @@ function inputSearch(event) {
   if (input.value) {
     let city = document.querySelector("#location");
     city.innerHTML = input.value;
+    let windUnits = document.querySelector("#windUnits");
+    windUnits.innerHTML = `m/s`;
     let apiKey = "1c79a9c19394dbdbf78cd6d4344cc928";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayData);
@@ -73,6 +78,8 @@ function inputSearch(event) {
 
 function getLocalData(position) {
   let currentPosition = `lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+  let windUnits = document.querySelector("#windUnits");
+  windUnits.innerHTML = `m/s`;
   let apiKey = "1c79a9c19394dbdbf78cd6d4344cc928";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?${currentPosition}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayData);
@@ -84,6 +91,8 @@ function getCurrentLocation() {
 
 function convertToCelsius() {
   let displayedCity = document.querySelector("#location");
+  let windUnits = document.querySelector("#windUnits");
+  windUnits.innerHTML = `m/s`;
   let apiCity = displayedCity.innerHTML;
   let apiKey = "1c79a9c19394dbdbf78cd6d4344cc928";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${apiCity}&appid=${apiKey}&units=metric`;
@@ -92,6 +101,8 @@ function convertToCelsius() {
 
 function getImperialData(currentCity) {
   let displayedCity = document.querySelector("#location");
+  let windUnits = document.querySelector("#windUnits");
+  windUnits.innerHTML = `mph`;
   let apiCity = displayedCity.innerHTML;
   let apiKey = "1c79a9c19394dbdbf78cd6d4344cc928";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${apiCity}&appid=${apiKey}&units=imperial`;
